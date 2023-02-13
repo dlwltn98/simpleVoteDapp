c233685d4e64f32c4bb7818b1c2c88dbbf1027a8
dlwltn98 <leejis0212@icloud.com>
2023-02-13 22:54:02 +0900

    simpleVote

---
+++ contracts/vote.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract vote {
+
+    struct Candidate {
+        string name;
+        uint256 upVote;
+    }
+
+    bool live;
+    address owner;
+    Candidate[] public candidateList;
+
+    mapping(address => bool) Voted;
+
+    event AddCandidate(string name);
+    event UpVote(string candidate, uint256 upVote);
+    event FinishVote(bool live);
+    event Voting(address owner);
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    modifier chkLive {  // 추가
+        require(live == true);
+        _;
+    }
+
+    constructor() {  // 수정
+        owner = msg.sender;
+        live = true;
+
+        emit Voting(owner);
+    }
+
+    // candidate
+    function addCandidate(string memory _name) public chkLive onlyOwner { // 수정
+        require(candidateList.length < 5);
+
+        candidateList.push(Candidate(_name, 0));
+
+        emit AddCandidate(_name);
+    }
+
+    //upVote
+    function upVote(uint256 _indexOfCandidate) public chkLive {
+        require(_indexOfCandidate < candidateList.length);
+        require(Voted[msg.sender] == false);
+
+        candidateList[_indexOfCandidate].upVote++;
+
+        Voted[msg.sender] = true;
+
+        emit UpVote(candidateList[_indexOfCandidate].name, candidateList[_indexOfCandidate].upVote);
+    } 
+
+    // finish vote
+    function finishVote() public chkLive onlyOwner {
+        live = false;
+
+        emit FinishVote(live);
+    }
+}
